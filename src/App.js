1411f3757ccf923405646bfa27fe0f90365b4e1b
이선우 <seonu2000@iseon-uui-MacBookAir.local>
2023-09-16 17:55:51 +0900

    location

--- src/App.js
+++ src/App.js
@@ -7,6 +7,48 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [map, setMap] = useState(null);
+  const [marker, setMarker] = useState(null);
+  const [userAddress, setUserAddress] = useState("Getting location...");
+
+  const getGeoLocation = (lat, lon) => {
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    const coord = new window.kakao.maps.LatLng(lat, lon);
+    geocoder.coord2Address(coord.getLng(), coord.getLat(), function (result, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        // 도로명 주소 대신 일반 주소를 사용
+        const detailAddr = result[0].address ? result[0].address.address_name : '';
+        setUserAddress(detailAddr);
+      }
+    });
+  };
+  
+  
+
+  const returnToMyLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const locPosition = new window.kakao.maps.LatLng(lat, lon);
+        if (map) {
+          map.setCenter(locPosition);
+          if (marker) {
+            marker.setMap(null);
+          }
+          const newMarker = new window.kakao.maps.Marker({
+            map: map,
+            position: locPosition
+          });
+          setMarker(newMarker);
+
+          getGeoLocation(lat, lon); // Update address
+        }
+      });
+    } else {
+      alert('Geolocation not supported');
+    }
+  };
 
   useEffect(() => {
     const container = document.getElementById('map');
@@ -14,16 +56,60 @@ function App() {
       center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
       level: 3
     };
-    new window.kakao.maps.Map(container, options);
+    const createdMap = new window.kakao.maps.Map(container, options);
+    setMap(createdMap);
+    
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const locPosition = new window.kakao.maps.LatLng(lat, lon);
+
+        createdMap.setCenter(locPosition);
+
+        const initialMarker = new window.kakao.maps.Marker({
+          map: createdMap,
+          position: locPosition
+        });
+        setMarker(initialMarker);
+
+        getGeoLocation(lat, lon); // Update address initially
+      });
+    }
+    
   }, []);
 
+
+
+const zoomIn = () => {
+  if (map) {
+    const level = map.getLevel();
+    map.setLevel(level - 1);
+  }
+};
+
+const zoomOut = () => {
+  if (map) {
+    const level = map.getLevel();
+    map.setLevel(level + 1);
+  }
+};
+
   const toggleNav = () => setNavOpen(!isNavOpen);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername("");
+    setPassword("");
+  };
 
   return (
     <div className="App">
       <div className="header">
         <img src="./logo1.png" alt="Header" className="logo" />
-        <h1>My Map Application</h1>
+        
+        <div className="user-address">{userAddress}</div>
       </div>
       <button className="toggle-button" onClick={toggleNav}>
         {isNavOpen ? 'Close Nav' : 'Open Nav'}
@@ -39,15 +125,28 @@ function App() {
               <Link to="/signup" className="signup-text">회원가입</Link>
             </div>
           ) : (
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
+            <div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+              <button onClick={handleLogout}>로그아웃</button>
+            </div>
           )}
         </div>
       )}
-      <div id="map" style={{ width: '100%', height: '80vh' }}></div>
+      <div id="map" style={{ width: '100%', height: '80vh' }}>
+      </div>
+      <button className="add-errand-button">심부름 추가하기</button>
+      <div className="zoom-button-container">
+        <button className="zoom-button" onClick={zoomIn}>줌 인</button>
+        <button className="zoom-button" onClick={zoomOut}>줌 아웃</button>
+        <button className="zoom-button" onClick={returnToMyLocation}>
+          내 위치
+        </button>
+      </div>
+      
     </div>
   );
 }
--- src/Signup.js
+++ src/Signup.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css';
 
 function Signup() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const handleSignup = () => {
-    // Add your signup logic here.
-    // After signup, navigate back to main page with the user logged in.
-    navigate('/', { replace: true });
+    setLoading(true);
+    // Add your signup logic here
+    // ...
+
+    // Simulate API call
+    setTimeout(() => {
+      setLoading(false);
+      navigate('/', { replace: true });
+    }, 2000);
   };
 
   return (
     <div className="signup-container">
       <div className="signup-form">
         <h1>회원가입</h1>
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button onClick={handleSignup}>회원가입</button>
+        <label htmlFor="username">Username <span className="required">*</span></label>
+        <input id="username" type="text" placeholder="Username" />
+        
+        <label htmlFor="email">Email <span className="required">*</span></label>
+        <input id="email" type="email" placeholder="Email" />
+        
+        <label htmlFor="password">Password <span className="required">*</span></label>
+        <input id="password" type="password" placeholder="Password" />
+        
+        {error && <div className="error">{error}</div>}
+        
+        <button disabled={loading} onClick={handleSignup}>
+          {loading ? 'Signing up...' : '회원가입'}
+        </button>
+
         <Link to="/" className="signup-link">Go back to main page</Link>
       </div>
     </div>
